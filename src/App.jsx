7fc8e595603b6e3e455b64afe301bc2ecd86e4fb
Eflowz <ezekielkachy@gmail.com>
2024-05-23 15:53:02 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./COMPONENTS/Navbar";
+import Hero from "./COMPONENTS/hero";
+import HomeCards from "./COMPONENTS/HomeCards";
+import Joblistings from "./COMPONENTS/joblistings";
+import ViewAll from "./COMPONENTS/viewAll";
+
+const App = () => {
+  return (
+  <>
+  <Navbar/>
+  <Hero/>
+  <HomeCards/>
+  <Joblistings/>
+  <ViewAll/>
+    </>
+  );
+};
+
+export default App;
